91f5070d0292be7a361ffe72c5ff8c23ec57b5bc
Wang Zhijie (Jack) <jwzhijie@gmail.com>
2022-10-01 09:28:00 +0800

    Add: basic layout of UI and routing of main pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './styles/App.css';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  MessageOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import "antd/dist/antd.css";
+import "./styles/App.css";
+import logo from "./logo.png";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Roles from "./components/Roles";
+import Skills from "./components/Skills";
+import Courses from "./components/Courses";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Content, Sider } = Layout;
+
+const App = () => (
+  <Layout>
+    <BrowserRouter>
+      <Sider breakpoint="lg">
+        <div className="brand">
+          <img src={logo} className="logo" /> All-in-One LJPS
+        </div>
+        <Menu mode="inline" defaultSelectedKeys={["Home"]}>
+          <Menu.Item icon={<AppstoreOutlined />} key="Home">
+            <Link to="/" className="menu-item-link">
+              Home
+            </Link>
+          </Menu.Item>
+          {[
+            [<BarChartOutlined />, "Roles"],
+            [<MessageOutlined />, "Skills"],
+            [<SettingOutlined />, "Courses"],
+          ].map((navItem) => (
+            <Menu.Item icon={navItem[0]} key={navItem[1] as string}>
+              <Link
+                to={(navItem[1] as string).toLowerCase()}
+                className="menu-item-link"
+              >
+                {navItem[1]}
+              </Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Sider>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/roles" element={<Roles />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/courses" element={<Courses />} />
+        </Routes>
+      </Content>
+    </BrowserRouter>
+  </Layout>
+);
 
 export default App;
---
+++ client/src/components/Skills.tsx
@@ -0,0 +1,8 @@
+export default function Skills() {
+  return (
+    <>
+      <h1>Skills</h1>
+      <p>Where I know</p>
+    </>
+  );
+}
