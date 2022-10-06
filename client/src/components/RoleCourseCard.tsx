9aa958d2de2accb160fa7f4a76c6495cc276c98d
Wang Zhijie (Jack) <jwzhijie@gmail.com>
2022-10-06 20:15:23 +0800

    G2T2-4 Update: Done frontend view for viewing all roles in organization during creation of learning journey

--- client/src/components/RoleCourseCard.tsx
+++ client/src/components/RoleCourseCard.tsx
@@ -3,16 +3,16 @@ import role from "../assets/role.png"
 
 export default function RoleCourseCard() {
   return (
-    <div style={{ width: "400" }}>
+    <div style={{ width: "50vw", justifyContent:"space-between", marginTop:"1vw"}} className="horizontal card">
       <div className="horizontal">
         <img src={role} alt="role icon" className="icon"/>
-        <div>
-          <h3>Role</h3>
+        <div className="card-row" style={{margin:"1vw 0 0 1.5vw"}}>
+          <p style={{fontSize:"1vw", fontWeight:"700"}}>Role</p>
           <p>Department:</p>
-          <p>Senority:</p>
+          <p>Description:</p>
         </div>
       </div>
-      <Button className="border cyan">Read More</Button>
+      <Button className="border btn-color">Read More</Button>
     </div>
   );
 }
--- client/src/pages/Roles.tsx
+++ client/src/pages/Roles.tsx
@@ -1,20 +1,30 @@
 import { Steps } from 'antd';
 import RoleCourseCard from '../components/RoleCourseCard';
+
 export default function Roles({ lj }: { lj?: boolean }) {
   return (
     <>
       <h1 style={{ fontWeight: 700 }}>Create Your Desired Learning Journey</h1>
 
-      <Steps labelPlacement='vertical' style={{width:"40vw", margin:"auto", marginTop:"5vh", marginBottom:"5vh"}}>
-        <Steps.Step title="Choose a role"/>
-        <Steps.Step title="Choose skills"/>
-        <Steps.Step title="Choose courses"/>
-      </Steps>
-      <h1 style={{textAlign:"center"}}>Roles Selected</h1>
+      <div
+        style={{
+          width: "50vw",
+          margin: "auto",
+          marginTop: "3vh",
+        }}
+      >
+        <Steps labelPlacement="vertical">
+          <Steps.Step title="Choose a role" />
+          <Steps.Step title="Choose skills" />
+          <Steps.Step title="Choose courses" />
+        </Steps>
+        <p style={{ textAlign: "center", margin:"3vh 0 4vh 0", color:"#3649F9", fontWeight:700 }}>Role Selected:</p>
 
-      <RoleCourseCard/>
-
-      
+        <RoleCourseCard />
+        <RoleCourseCard />
+        <RoleCourseCard />
+        <RoleCourseCard />
+      </div>
     </>
   );
 }
