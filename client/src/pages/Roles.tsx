59c9e68257aec5d524448f764a865b18ef3b19ab
jordanteoh2020 <jordan.teoh28@gmail.com>
2022-10-25 16:31:03 +0800

    G2T2-26 Add: Implementing Role cards and populating data

--- client/src/pages/Roles.tsx
+++ client/src/pages/Roles.tsx
@@ -1,8 +1,41 @@
+import { PropertySafetyFilled } from "@ant-design/icons";
+import { Row, Col, Button, Pagination, Steps } from "antd";
+import RoleCourseCard from "../components/RoleCourseCard";
+import Header from '../components/RolesHeader';
+import CourseCard from "../components/SkillCard";
+import { Role } from "../types/Role";
+import { Course } from "../types/Course";
+
+
 export default function Roles() {
+
+
+
+
   return (
     <>
       <h1>Roles</h1>
       <p>Truly</p>
+<div style={}>
+
+
+
+
+      <div>
+      <RoleCourseCard handleClick={() => {}}/>
+      
+      </div>
+      <div>
+      <RoleCourseCard handleClick={() => {}}  />
+      </div>
+      <div>
+      <RoleCourseCard handleClick={() => {}}  />
+      </div>
+      <div>
+      <RoleCourseCard handleClick={() => {}}  />
+      </div>
+
+      </div>
     </>
   );
 }
