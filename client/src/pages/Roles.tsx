afcb92c55164cece59a5c54b1e355b27200767da
jordanteoh2020 <jordan.teoh28@gmail.com>
2022-11-01 09:58:48 +0800

    G2T2-88 Add: Populate Role Card according to Position Data

--- client/src/pages/Roles.tsx
+++ client/src/pages/Roles.tsx
@@ -1,34 +1,41 @@
-import { Row, Button } from 'antd';
+import { Row, Input, Col } from 'antd';
 import { useState, useEffect } from "react";
 import axios from "axios";
 import RoleCourseCard from "../components/RoleCourseCard";
 import { Role } from "../types/Role";
-
+import styles from "../styles/RenderRoleCourseCard.module.css";
 
 export default function Roles() {
 
-  // const [roles, setRoles] = useState<Role[]>([]);
-
-  // useEffect(() => {
-  //   axios
-  //   .get("http://localhost:5000/positions/all")
-  //   .then((resp) => setRoles(resp.data.data))
-  //   .catch((err) => console.log(err));
-  // }, []);
-
-  // console.log(roles);
+    const [roles, setRoles] = useState<Role[]>([]);
+  
+    useEffect(() => {
+      axios
+      .get("http://localhost:5000/positions/active")
+      .then((resp) => setRoles(resp.data.data))
+      .catch((err) => console.log(err));
+    }, []);
 
   return (
     <>
-      <h1>Available Roles</h1>
-      {/* <div style={{  width: '50vw', margin: 'auto', marginTop: '10vh' }}>
-        <Row style={{ justifyContent: 'flex-end', marginBottom: '5vh' }}>
-          <Button type="primary">Create role</Button>
+      <Row>
+        <Col>
+          <h1>Available Roles</h1>
+        </Col>
+        <Col style={{paddingTop: '0.5vh'}} offset={1}>
+          <Input placeholder="Enter search" className={styles.search}/>
+        </Col>
+      </Row>
+      <div style={{  width: '50vw', margin: 'auto', marginTop: '10vh' }}>
+        <Row style={{ marginBottom: '5vh' }}>
+          <b>
+            {roles.length} Roles Displayed
+          </b>
         </Row>
-      {roles.map((role) => (
-        <RoleCourseCard role={role} edit={true} />
+      {roles && roles.map((role) => (
+        <RoleCourseCard role={role}/>
       ))}
-      </div> */}
+      </div>
     </>
   );
 }
