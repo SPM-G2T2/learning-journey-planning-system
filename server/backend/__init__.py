d90fb91c8e70fcac6d1a65bc0c83c6f58a6d9f54
hackyjacky <67328565+hackyjacky@users.noreply.github.com>
2022-11-02 17:48:09 +0800

    Refactor: Accessing environment variables

--- server/backend/__init__.py
+++ server/backend/__init__.py
@@ -9,12 +9,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config['SECRET_KEY'] = '1234567890'
 
-    password = os.environ.get("PASSWORD")
-
-    if not password:
-        raise ValueError("No Password Set")
+    password = os.environ["PASSWORD"]
 
     # Mac
     # authDetails = "root:root@localhost:8889"
