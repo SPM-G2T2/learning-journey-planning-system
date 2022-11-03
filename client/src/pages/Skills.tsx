8543ecddb08ddd3422befda7535af47525f9a499
Joey Lau <joeylau.2020@scis.smu.edu.sg>
2022-11-03 11:31:11 +0800

    G2T2-89 Update: UI Flow

--- client/src/pages/Skills.tsx
+++ client/src/pages/Skills.tsx
@@ -1,12 +1,15 @@
-import { Row, Col, Button, Pagination, Steps } from "antd";
+import { Row, Col, Button, Pagination, Input} from "antd";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Header from "../components/RolesHeader";
 import SkillCard from "../components/SkillCard";
 import { Skill } from "../types/Skill";
+import styles from "../styles/RenderHRCard.module.css";
 
 export default function Skills() {
+
   const [skills, setSkills] = useState<Skill[]>([]);
+  const [searchedCourses, setSearchedCourses] = useState<Skill[]>([]);
+  const [search, setSearch] = useState<boolean>(false);
 
   useEffect(() => {
     axios
@@ -17,90 +20,29 @@ export default function Skills() {
 
   return (
     <>
-      <Header />
-
-      <div
-        style={{
-          width: "50vw",
-          margin: "auto",
-          marginTop: "3vh",
-        }}
-      >
-        <Steps current={1} labelPlacement="vertical">
-          <Steps.Step title="Choose a role" />
-          <Steps.Step title="Choose skills" />
-          <Steps.Step title="Choose courses" />
-        </Steps>
-        <div style={{ color: "#3649F9", marginBottom: 20, marginTop: 20 }}>
-          <Row gutter={[10, 10]}>
-            <Col
-              span={12}
-              style={{
-                textAlign: "right",
-                fontWeight: 700,
-              }}
-            >
-              Role Selected:
-            </Col>
-            <Col span={12} style={{ textAlign: "left", fontWeight: 450 }}>
-              Role Name
-            </Col>
-            <Col
-              span={12}
-              style={{
-                textAlign: "right",
-                fontWeight: 700,
-              }}
-            >
-              Skills Selected:
-            </Col>
-            <Col span={12} style={{ textAlign: "left", fontWeight: 450 }}>
-              Skill Name
-            </Col>
-          </Row>
-        </div>
+     <Row>
+        <Col>
+          <h1>Available Skills</h1>
+        </Col>
+        <Col style={{paddingTop: '0.2vh'}} offset={1}>
+          <Input placeholder="Enter search" className={styles.search}/>
+        </Col>
+      </Row>
+      <div style={{  width: '50vw', margin: 'auto', marginTop: '10vh' }}>
+        <Row style={{ marginBottom: '5vh' }}>
+          <b>
+            {skills.length} Skills Displayed
+          </b>
+        </Row>
       </div>
       <Row gutter={[16, 24]}>
         {skills &&
           skills.map((skill) => (
             <Col span={8}>
-              <SkillCard skill={skill} lj={false} />
+              <SkillCard skill={skill} lj={true}/>
             </Col>
           ))}
-      </Row>
-      <Row style={{ marginTop: "6vh", fontWeight: 700 }}>
-        <Col span={12} style={{ textAlign: "right" }}>
-          <Pagination defaultCurrent={1} total={50} />
-        </Col>
-        <Col offset={4} span={3} style={{ textAlign: "right" }}>
-          <Button
-            type="default"
-            style={{
-              fontSize: 14,
-              fontWeight: 500,
-              color: "black",
-              background: "",
-              borderRadius: 5,
-            }}
-          >
-            Back
-          </Button>
-        </Col>
-        <Col offset={1} span={3} style={{ textAlign: "left" }}>
-          <Button
-            className="border btn-color"
-            style={{
-              fontSize: 14,
-              fontWeight: 500,
-              color: "black",
-              background: "",
-              borderRadius: 5,
-            }}
-          >
-            Next
-          </Button>
-        </Col>
-      </Row>
+        </Row>
     </>
   );
 }
