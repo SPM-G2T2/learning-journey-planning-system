09e4878ce4f67b907ee84ac7f8c42180b65f58d9
xiaozhang8888 <92302698+xiaozhang8888@users.noreply.github.com>
2022-11-05 22:26:22 +0800

    G2T2-89 Add: Changed parts of the UI to make it more consistent with other pages

--- client/src/pages/Skills.tsx
+++ client/src/pages/Skills.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Button, Pagination, Input} from "antd";
+import { Row, Input, Col } from 'antd';
 import { useState, useEffect } from "react";
 import axios from "axios";
 import SkillCard from "../components/SkillCard";
@@ -6,11 +6,11 @@ import { Skill } from "../types/Skill";
 import styles from "../styles/RenderHRCard.module.css";
 
 export default function Skills() {
-
+  const [step] = useState<number>(0);
   const [skills, setSkills] = useState<Skill[]>([]);
-  const [searchedCourses, setSearchedCourses] = useState<Skill[]>([]);
+  const [searchedSkills, setSearchedSkills] = useState<Skill[]>([]);
   const [search, setSearch] = useState<boolean>(false);
-
+  
   useEffect(() => {
     axios
       .get("http://localhost:5000/skills/active")
@@ -18,31 +18,47 @@ export default function Skills() {
       .catch((err) => console.log(err));
   }, []);
 
-  return (
+    function handleChange(event: any) {
+      console.log(event.target.value);
+      console.log(event.target.value.length);
+      var tempSearchedRoles = [];
+      for (let skill of skills) {
+          if (skill.skill_name.toLowerCase().includes(event.target.value.toLowerCase())) {
+            tempSearchedRoles.push(skill);
+          }
+      }
+      setSearchedSkills(tempSearchedRoles);
+      console.log(searchedSkills);
+      setSearch(true);
+      if (event.target.value.length === 0) {
+          setSearch(false);
+      } 
+    }
+
+    return (
     <>
-     <Row>
+      <Row>
         <Col>
           <h1>Available Skills</h1>
         </Col>
         <Col style={{paddingTop: '0.2vh'}} offset={1}>
-          <Input placeholder="Enter search" className={styles.search}/>
+          <Input placeholder="Enter search" className={styles.search} onChange={handleChange}/>
         </Col>
       </Row>
       <div style={{  width: '50vw', margin: 'auto', marginTop: '10vh' }}>
         <Row style={{ marginBottom: '5vh' }}>
           <b>
-            {skills.length} Skills Displayed
+            {search ? searchedSkills.length : skills.length} Roles Displayed
           </b>
         </Row>
+      <Row gutter={[0, 30]}>
+      { search ? searchedSkills && searchedSkills.map((searchedSkill) => (
+        <SkillCard skill={searchedSkill} lj={false}/>
+      )) : skills && skills.map((skill) => (
+        <SkillCard skill={skill} lj={false}/>
+      )) }
+      </Row>
       </div>
-      <Row gutter={[16, 24]}>
-        {skills &&
-          skills.map((skill) => (
-            <Col span={8}>
-              <SkillCard skill={skill} lj={true}/>
-            </Col>
-          ))}
-        </Row>
     </>
   );
 }
